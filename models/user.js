f2100f460147803205059ebdc1c7d58e65bfbb06
Jesse Abarca <JesseAbarca888@gmail.com>
2023-08-01 23:23:02 -0700

    Create the models folder and add a user.js with User schemma

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
