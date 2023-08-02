b562f728b806c4fd1117b9d0ab5cb95e37e7d913
Jesse Abarca <JesseAbarca888@gmail.com>
2023-08-02 00:18:15 -0700

    index.js now only handles the server

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const express = require("express");
-require('dotenv').config();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const app = express();
+const app = require("./app");
 const port = 3000;
-
-// Middleware setup
-app.use(bodyParser.json());
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
